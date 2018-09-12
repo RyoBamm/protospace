98c3a014c0f186bad1a8e4ed1cf88df186a1521b
User <user@yuzanoMacBook-ea.local>
2018-09-12 10:06:23 +0900

    edit_migration-files

--- db/migrate/20180912003338_create_prototype_tags.rb
+++ db/migrate/20180912003338_create_prototype_tags.rb
@@ -1,7 +1,8 @@
 class CreatePrototypeTags < ActiveRecord::Migration
   def change
     create_table :prototype_tags do |t|
-
+      t.references :prototype, foreign_key: true, null: false
+      t.references :tag, foreing_key: true, null: false
       t.timestamps null: false
     end
   end
