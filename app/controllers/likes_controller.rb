de60ee263cd84feffef2f0e4d538f81518ecdf97
Yusuke Murakami <tropical.inu@gmail.com>
2018-09-18 12:03:21 +0900

    initial commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,12 @@ class LikesController < ApplicationController
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototypes = Prototype.all
-    # respond_to do |format|
-    # format.html
-    # format.json
+    respond_to do |format|
+    format.html
+    format.json
+    end
     # render "show"
-    redirect_to :root, notice: 'like was successfully created'
+    # redirect_to :root, notice: 'like was successfully created'
   end
 
   def destroy
@@ -15,10 +16,11 @@ class LikesController < ApplicationController
     like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototypes = Prototype.all
-    # respond_to do |format|
-    # format.html
-    # format.json
+    respond_to do |format|
+    format.html
+    format.json
+    end
     # render "show"
-    redirect_to :root, notice: 'like was successfully deleted'
+    # redirect_to :root, notice: 'like was successfully deleted'
   end
 end
