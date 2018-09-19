aa34cf6456b9bad32c20181c3d193dadcc9cba6b
Yusuke Murakami <tropical.inu@gmail.com>
2018-09-19 09:29:17 +0900

    new-image_contoroller_view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,11 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
-    respond_to do |format|
-    format.html
-    format.json
-    end
+    @prototype.reload
+  
     # render "show"
     # redirect_to :root, notice: 'like was successfully created'
   end
@@ -15,12 +14,15 @@ class LikesController < ApplicationController
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
-    respond_to do |format|
-    format.html
-    format.json
-    end
+    @prototype.reload
     # render "show"
     # redirect_to :root, notice: 'like was successfully deleted'
   end
+
+  private
+
+  def set_post
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
 end
