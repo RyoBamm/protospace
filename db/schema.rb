196f4209c33ecf93ab967889c19c55666091b659
Yusuke Murakami <tropical.inu@gmail.com>
2018-09-22 13:47:26 +0900

    comment-out

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20180913011211) do
 
   add_index "captured_images", ["prototype_id"], name: "index_captured_images_on_prototype_id"
 
-
   create_table "prototype_tags", force: :cascade do |t|
     t.integer  "prototype_id", null: false
     t.integer  "tag_id",       null: false
@@ -32,13 +31,24 @@ ActiveRecord::Schema.define(version: 20180913011211) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
+    t.integer  "user_id"
+    t.integer  "prototype_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "prototype_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    
+  end
+
+  create_table "prototype_tags", force: :cascade do |t|
+    t.integer  "prototype_id", null: false
+    t.integer  "tag_id",       null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "prototypes", force: :cascade do |t|
